9ff79a7f157a10e80e5e8a53f9b70cdb05399609
plkv <stasplkv@gmail.com>
2025-09-17 01:50:56 +0200

    Revert "Add Vercel Analytics tracking"
    This reverts commit efbd959b207ed1e67f61acc775917386b15f2888.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Space_Grotesk, Space_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 
 const inter = Inter({
@@ -93,10 +92,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-sans antialiased">
-        {children}
-        <Analytics />
-      </body>
+      <body className="font-sans antialiased">{children}</body>
     </html>
   )
 }
